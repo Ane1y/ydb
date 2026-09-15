a427bc51d57d064f61c8616da4679c6fa64529a5
Anelya Dusaeva <anely-d@ydb.tech>
2026-09-15 11:36:18 +0000

    expose query resources and log AST

--- ydb/core/kqp/executer_actor/kqp_executer_impl.h
+++ ydb/core/kqp/executer_actor/kqp_executer_impl.h
@@ -943,6 +943,9 @@ protected:
                 StatCollectInflightBytes = collectBytes;
                 Counters->Counters->QueryStatCpuCollectUs->Add(deltaCpuTime * 1'000'000);
             }
+            if (auto currentStats = GetUserRequestContext()->CurrentQueryStats) {
+                Stats->ReportCurrentStats(*currentStats);
+            }
             ProcessStreamingQueryCounters();
         }
 
@@ -1603,7 +1606,7 @@ protected:
             .UserToken = UserToken,
             .Deadline = Deadline.GetOrElse(TInstant::Zero()),
             .StatsMode = Request.StatsMode,
-            .WithProgressStats = Request.ProgressStatsPeriod != TDuration::Zero(),
+            .WithProgressStats = Request.ProgressStatsPeriod != TDuration::Zero() || bool(GetUserRequestContext()->CurrentQueryStats),
             .RlPath = Request.RlPath,
             .ExecuterSpan =  ExecuterSpan,
             .ResourcesSnapshot = std::move(ResourcesSnapshot),
@@ -1916,6 +1919,9 @@ protected:
             ReportEventElapsedTime();
 
             Stats->FinishTs = TInstant::Now();
+            if (auto currentStats = GetUserRequestContext()->CurrentQueryStats) {
+                Stats->ReportCurrentStats(*currentStats, true);
+            }
 
             {
                 ui64 cycleCount = GetCycleCountFast();
